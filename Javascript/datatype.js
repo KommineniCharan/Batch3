8df01923f42e238bcefd2ba5d38c3677dcf9e1f3
KommineniCharan <charank347@gmail.com>
2024-08-20 09:05:03 +0530

    Datatypes

---
+++ Javascript/datatype.js
@@ -0,0 +1,27 @@
+var empname = "Rahul";
+console.log(empname);
+console.log(typeof empname);
+
+var x = 15.6;
+console.log(x);
+console.log(typeof x);
+
+var y = true;
+console.log(y);
+console.log(typeof y);
+
+var z = null;
+console.log(z);
+console.log(typeof z);
+
+var k;
+console.log(k);
+console.log(typeof k);
+
+var l = 99999999999855656555n;
+console.log(l);
+console.log(typeof l);
+
+var o = Symbol("description");
+console.log(o);
+console.log(typeof o);
