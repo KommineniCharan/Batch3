ce673d426419fe93417947bf858787eeec9d778b
KommineniCharan <charank347@gmail.com>
2024-08-28 09:02:31 +0530

    Operators

---
+++ Javascript/operators.js
@@ -0,0 +1,20 @@
+//Arithematic Operator
+var a = 30;
+var b = 40;
+//console.log(a + b);
+var c = a + b;
+//console.log(c);
+//console.log("The Values Of a + b are : ", a + b);
+console.log(`The Values of a + b are ${a + b}`);
+
+var d = b - a;
+console.log("The values of b - a is : ", d);
+
+var e = a * b;
+console.log("The values of a * b is : ", e);
+
+var f = a / b;
+console.log("The values of a / b is : ", f);
+
+var g = b % a;
+console.log("The values of b % a is : ", g);
